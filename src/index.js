6397ef7fa6aa5bbfef4d2e8b6a588963987cfed9
Daniel <danbapp@gmail.com>
2021-11-08 15:36:00 -0500

    Assignment Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  console.log('DOMContentLoaded has loaded')
 });
+
+let form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  // console.log(e.target.new-task-description.value)
+  form.reset()
+})
+
+function buildTodo(todo) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  p.textContent= todo
+  btn.textContent = 'x'
+  document.querySelector('#tasks').appendChild(p)
+  p.appendChild(btn)
+  console.log(p)
+}
